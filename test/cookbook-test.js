71713448e4474bfa9ed399f85fb5567f376150d5
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-09-14 22:32:10 -0400

    Refactor cookbook test to include ingredients

--- test/cookbook-test.js
+++ test/cookbook-test.js
@@ -2,21 +2,22 @@ import {expect} from 'chai';
 
 
 import recipeData from '../src/data/recipes';
+import ingredientData from '../src/data/ingredients';
 import Cookbook from '../src/cookbook';
 
 let cookbook;
 
 describe('Cookbook', () => {
   beforeEach(() => {
-    cookbook = new Cookbook(recipeData);
+    cookbook = new Cookbook(recipeData, ingredientData);
   });
 
   it('Should be a function', () => {
-   expect(Cookbook).to.be.a('function');
+    expect(Cookbook).to.be.a('function');
   });
 
   it('Should be an instance of Cookbook', () => {
-   expect(cookbook).to.be.an.instanceof(Cookbook)
+    expect(cookbook).to.be.an.instanceof(Cookbook)
   });
 
   it('Should have an array of all recipes', () => {
